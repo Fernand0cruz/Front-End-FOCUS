7563827621af20ed4e17a66c5720ea4eeb86dc3f
Luiz Fernando <cruzluizfernando097@gmail.com>
2021-05-27 14:43:58 -0300

    first commit

--- js/off-canvas.js
+++ js/off-canvas.js
@@ -4,24 +4,23 @@ document.querySelector('.close-menu').addEventListener('click', CloseOffCanvas)
 var OpenMenu = document.querySelector('.menu')
 var CloseMenu = document.querySelector('.close-menu')
 
-function OpenOffCanvas(){
+function OpenOffCanvas() {
     document.querySelector('.off-canva').style.left = '0'
     OpenMenu.style.display = 'none'
     CloseMenu.style.display = 'block'
 }
-function CloseOffCanvas(){
-    
+
+function CloseOffCanvas() {
+
     if (window.matchMedia("(max-width:600px)").matches) {
         document.querySelector('.off-canva').style.left = '-70%'
         CloseMenu.style.display = 'none'
         OpenMenu.style.display = 'block'
-        /* a viewport tem pelo menos 800 pixels de largura */
         // console.log('a viewport tem menos de 600 pixels de largura')
-      } else {
-          document.querySelector('.off-canva').style.left = '-30%'
-          CloseMenu.style.display = 'none'
-          OpenMenu.style.display = 'block'
-          /* a viewport menos que 800 pixels de largura */
-          // console.log('a viewport tem mais que 600 pixels de largura')
+    } else {
+        document.querySelector('.off-canva').style.left = '-30%'
+        CloseMenu.style.display = 'none'
+        OpenMenu.style.display = 'block'
+        // console.log('a viewport tem mais que 600 pixels de largura')
     }
 }
